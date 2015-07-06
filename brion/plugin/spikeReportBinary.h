e29d80097d4faebf5f17f41782dd0d89f902dc49
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-07-06 13:16:20 +0200

    Document binary spike report format

--- brion/plugin/spikeReportBinary.h
+++ brion/plugin/spikeReportBinary.h
@@ -28,7 +28,15 @@ namespace brion
 namespace plugin
 {
 
-/** A Binary spike report reader. */
+/**
+ * A Binary spike report reader.
+ *
+ * The format read by this plugin is:
+ * - 4b integer: magic '0xf0a'
+ * - 4b integer: version, currently '1'
+ * - (4b float, 4b integer) pairs until end of file: spike time, neuron GID,
+ *   sorted by time
+ */
 class SpikeReportBinary : public SpikeReportPlugin
 {
 public:
