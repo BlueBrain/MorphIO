c6c1f27f8452f3ac74a021614e40be941b57ff63
Adrien Devresse <adrien.devresse@epfl.ch>
2017-01-10 11:21:40 +0100

    Introduce initial spatial indexer for morphology element

--- src/morpho_spatial.cpp
+++ src/morpho_spatial.cpp
@@ -1,7 +1,25 @@
 #include "morpho_spatial.hpp"
 
-morpho_spatial::morpho_spatial()
+
+namespace morpho{
+
+class spatial_index_impl{
+public:
+
+
+    std::vector<std::shared_ptr<morpho_tree> > morphos;
+};
+
+spatial_index::spatial_index() : _pimpl(new spatial_index_impl())
 {
 
 }
+
+
+void spatial_index::add_morpho_tree(const std::shared_ptr<morpho_tree> &tree){
+    _pimpl->morphos.push_back(tree);
+}
+
+
+} // morpho
 
