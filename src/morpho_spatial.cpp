f9a35de800fd4f5d36baf46fbbda82cdeaf305c2
Adrien Devresse <adrien.devresse@epfl.ch>
2017-01-10 11:21:08 +0100

    Update hadoken

---
+++ src/morpho_spatial.cpp
@@ -0,0 +1,7 @@
+#include "morpho_spatial.hpp"
+
+morpho_spatial::morpho_spatial()
+{
+
+}
+
---
+++ src/morpho_spatial.hpp
@@ -0,0 +1,11 @@
+#ifndef MORPHO_SPATIAL_H
+#define MORPHO_SPATIAL_H
+
+
+class morpho_spatial
+{
+public:
+    morpho_spatial();
+};
+
+#endif // MORPHO_SPATIAL_H
