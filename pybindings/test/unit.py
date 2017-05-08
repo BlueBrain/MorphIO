a3e7d162a5f327545e8a083fbe7767a2d3dfcf79
Fernando Pereira <fernando.pereira@epfl.ch>
2017-05-08 18:02:58 +0200

    Python tests: Implementing all morphology tests similar to C++. All good

--- pybindings/test/unit.py
+++ pybindings/test/unit.py
@@ -2,11 +2,16 @@ from __future__ import absolute_import
 import unittest
 from morphotool.morphotool import *
 import numpy as np
+from numpy import testing as nptest
+import os
+
+_CUR_DIR = os.path.dirname(__file__)
+_H5V1_MORPHO_TEST_FILE = os.path.join(_CUR_DIR, "dend-C090905B_axon-sm110125a1-3_idA.h5")
+
 
 class TestMorphoTree(unittest.TestCase):
 
     def test_mtree(self):
-
         tree = MorphoTree()
         assert tree.tree_size == 0
 
@@ -50,13 +55,77 @@ class TestMorphoTree(unittest.TestCase):
         assert tree.get_parent(1) == 0
         assert tree.get_parent(0) == -1
 
-        # test swapmethod
+        # test swap method
         tree_copy = MorphoTree()
         assert tree_copy.tree_size == 0
 
         tree_copy.swap(tree)
         assert tree_copy.tree_size == 2
         assert tree.tree_size == 0
+
+
+class Test_H5V1(unittest.TestCase):
+    def setUp(self):
+        reader = MorphoReader(_H5V1_MORPHO_TEST_FILE)
+        self.tree = reader.create_morpho_tree()
+        # force destroy the loader to check any data dependencies
+
+    def test_reader(self):
+        tree = self.tree
+        reader = MorphoReader(_H5V1_MORPHO_TEST_FILE)
+
+        reader_structs = reader.struct_raw
+        self.assertEqual(len(reader_structs), tree.tree_size)
+        self.assertTrue(tree.get_node(0).is_of_type(MORPHO_NODE_TYPE.neuron_soma_type))
+
+        raw_points = reader.points_raw
+        soma = tree.get_node(0)
+        self.assertTrue(soma.branch_type == NEURON_STRUCT_TYPE.soma)
+        self.assertEqual(len(soma.line_loop), reader.get_branch_range_raw(0)[1])
+
+        # We keep C++ style to avoid introducing mismatches to the original version
+        for i in range(1, tree.tree_size):
+            nbranch = tree.get_node(i)
+            self.assertTrue(nbranch.is_of_type(MORPHO_NODE_TYPE.neuron_branch_type))
+            branch_raw_offset = reader.get_branch_range_raw(i)[0]
+            branch_raw_size = reader.get_branch_range_raw(i)[1]
+            self.assertEqual(nbranch.number_points, branch_raw_size)
+            self.assertEqual(len(nbranch.points), branch_raw_size)
+            self.assertEqual(len(nbranch.radius), branch_raw_size)
+            self.assertTrue(nbranch.branch_type in (NEURON_STRUCT_TYPE.axon,
+                                                    NEURON_STRUCT_TYPE.dentrite_basal,
+                                                    NEURON_STRUCT_TYPE.dentrite_apical))
+
+            for j in range(nbranch.number_points):
+                # Raw points map to Numpy structures -> cool for indexing and math
+                point_raw = (raw_points[branch_raw_offset+j, 0:3])
+                radius_raw = raw_points[branch_raw_offset+j, 3] / 2
+                nptest.assert_array_almost_equal(point_raw, nbranch.points[j])
+                nptest.assert_array_almost_equal(radius_raw, nbranch.radius[j])
+
+    @staticmethod
+    def _is_inside_box(box, point):
+        b_min = box.min_corner
+        b_max = box.max_corner
+        for i in range(3):
+            if b_min[i] > point[i]:
+                return False
+            if b_max[i] < point[i]:
+                return False
+        return True
+
+    def test_bounding_box(self):
+        global_tree_box = self.tree.bounding_box
+        for node in self.tree.all_nodes:
+            for point in (node.pointsVector if isinstance(node, NeuronBranch) else node.line_loop):
+                self.assertTrue(global_tree_box, point)
+
+    def test_spatial(self):
+        index = SpatialIndex()
+        index.add_morpho_tree(self.tree)
+        for node in self.tree.all_nodes[1:]:
+            for point in (node.pointsVector if isinstance(node, NeuronBranch) else node.line_loop):
+                self.assertTrue(index.is_within(point))
 
 
 if __name__ == "__main__":
