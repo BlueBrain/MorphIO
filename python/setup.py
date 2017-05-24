a290803fb6a3248b4bda64b3659063b57cab6f68
Fernando Pereira <fernando.pereira@epfl.ch>
2017-05-24 17:13:46 +0200

    CMake building Pyhton sdist and bdist distributable packages

--- python/setup.py
+++ python/setup.py
@@ -5,20 +5,6 @@ import os.path
 from distutils.core import setup, Extension
 
 
-def find_packages(where="."):
-    """Short version of setuptools.find_packages"""
-    all_pkgs=[]
-    for root, dirs, files in os.walk(where, followlinks=True):
-        all_dirs = dirs[:]; dirs[:] = []
-        for cdir in all_dirs:
-            full_path = os.path.join(root, cdir)
-            package   = os.path.relpath(full_path, where).replace(os.path.sep, '.')
-            if '.' not in cdir and os.path.isfile(os.path.join(full_path, '__init__.py')):
-                all_pkgs.append(package)
-                dirs.append(cdir)
-    return all_pkgs
-
-
 MORPHOTOOL_EXT = Extension('morphotool.morphotool',
     libraries = ['morpho'],
     sources = ['morphotool/morphotool.cpp'],
@@ -36,7 +22,7 @@ setup_opts = dict(
     download_url = '',
     platforms    = ['Mac OS X', 'Linux'],
     license      = 'GNU General Public License Version 3.0',
-    packages     = find_packages(),
+    packages     = ["morphotool"],
     ext_modules  = [MORPHOTOOL_EXT]
 )
 
