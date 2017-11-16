aa413dc1b08bead465491785b9fe8e357d5e58ce
Juan Hernando Vieites <juan.hernando@epfl.ch>
2017-11-16 15:35:41 +0100

    Fix Python wheel generation.
    Added an option to be used by Jenkins to choose the destination channel
    for uploading the wheel.

--- packaging/python_wheel/upload_wheel.sh
+++ packaging/python_wheel/upload_wheel.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 BASE=$(git rev-parse --show-toplevel)
 
+if [ "$RELEASE" == "true" ]; then
+    CHANNEL=release
+else
+    CHANNEL=dev
+fi
+
 for wheel in `ls $BASE/packaging/python_wheel/wheelhouse/brain*.whl`; do
-    upload2repo -t python -r dev -f $wheel
+    upload2repo -t python -r $CHANNEL -f $wheel
 done
 
 # cleanup afterwards
