b8c4bb5e38af6611c746084bb183a292097dbd76
Benoit Coste <benoit.coste@epfl.ch>
2018-02-28 16:39:25 +0100

    SWC reader cleanup

--- src/plugin/morphologySWC.cpp
+++ src/plugin/morphologySWC.cpp
@@ -21,25 +21,22 @@ const int SWC_UNDEFINED_PARENT = -1;
 
 struct Sample
 {
-    Sample()
-        : valid(false)
-        , type(SECTION_UNDEFINED)
-        , parentId(-1)
-    {
-    }
+    Sample() : valid(false) , type(SECTION_UNDEFINED) , parentId(-1)
+        {
+        }
 
     explicit Sample(const char* line)
-    {
-        float radius;
-        valid = sscanf(line, "%20d%20d%20f%20f%20f%20f%20d", (int*)&id, (int*)&type, &point[0],
-                       &point[1], &point[2], &radius, &parentId) == 7;
+        {
+            float radius;
+            valid = sscanf(line, "%20d%20d%20f%20f%20f%20f%20d", (int*)&id, (int*)&type, &point[0],
+                           &point[1], &point[2], &radius, &parentId) == 7;
 
-        diameter = radius * 2; // The point array stores diameters.
+            diameter = radius * 2; // The point array stores diameters.
 
-        if (type >= SECTION_CUSTOM_START)
-            valid = false; // Unknown section type, custom samples are also
-                           // Regarded as unknown.
-    }
+            if (type >= SECTION_CUSTOM_START)
+                valid = false; // Unknown section type, custom samples are also
+            // Regarded as unknown.
+        }
 
     float diameter;
     bool valid;
@@ -60,12 +57,12 @@ std::string errorMsg(const std::string& filename,
         additionalMessage;
 }
 
-std::map<uint32_t, Sample> _readSamples(const URI& uri)
+const std::map<uint32_t, Sample> _readSamples(const URI& uri)
 {
     std::ifstream file(uri.c_str());
     if (file.fail())
         LBTHROW(morphio::RawDataError("Error opening morphology file: " +
-                                        uri));
+                                      uri));
 
     std::map<uint32_t, Sample> samples;
 
@@ -87,76 +84,108 @@ std::map<uint32_t, Sample> _readSamples(const URI& uri)
     return samples;
 }
 
-std::ostream& operator<<(std::ostream& os, const morphio::Point& point)
+
+/**
+   Parsing SWC according to this specification:
+   http://www.neuronland.org/NLMorphologyConverter/MorphologyFormats/SWC/Spec.html
+**/
+class SWCBuilder
 {
-    os <<  point[0] << ' ' << point[1] << ' ' << point[2];
-    return os;
-}
+public:
+    SWCBuilder(const std::map<uint32_t, Sample>& _samples) : samples(_samples)
+        {
+            lastSomaPoint = -1;
+            for(auto sample_pair: samples){
+                const auto &sample = sample_pair.second;
+                children[sample.parentId].push_back(sample.id);
+                if(sample.type == SECTION_SOMA)
+                    lastSomaPoint = sample.id;
+            }
+        }
 
 
-
-Property::Properties _buildProperties(const std::map<uint32_t, Sample> &samples){
-    std::map<uint32_t, std::vector<uint32_t>> children;
-
-    // Dictionnary: SWC Id of the last point of a section to section ID
-    std::map<uint32_t, uint32_t> swcIdToSectionId;
-    int lastSomaPoint = -1;
-    for(auto sample_pair: samples){
-        const auto &sample = sample_pair.second;
-        children[sample.parentId].push_back(sample.id);
-        if(sample.type == SECTION_SOMA)
-            lastSomaPoint = sample.id;
+    bool isSectionStartButSoma(const Sample& sample) {
+        return properties._points.size() == 0 && // Start of a new section
+            sample.parentId != SWC_UNDEFINED_PARENT; // filter out soma section
     }
 
-    mut::Morphology morph;
+    bool isSectionEnd(const Sample& sample) {
+            return sample.id == lastSomaPoint || // End of soma
+                children[sample.id].size() == 0 || // Reached leaf
+                children[sample.id].size() >= 2; // Reached bifurcation
+        }
 
-    Property::PointLevel properties;
-    int32_t parentSectionId = -1;
+    void appendSample(const Sample& sample) {
+        properties._points.push_back(sample.point);
+        properties._diameters.push_back(sample.diameter);
+    }
 
-    for(auto sample_pair: samples) {
-        const auto &sample = sample_pair.second;
-        // Empty property means the start of a new section
-        if(properties._points.size() == 0 &&
-           sample.parentId != SWC_UNDEFINED_PARENT) // Filter out soma section
+    Property::Properties _buildProperties()
+        {
+            for(auto sample_pair: samples) {
+                const auto &sample = sample_pair.second;
+
+                if(isSectionStartButSoma(sample))
+                    _processSectionStart(sample);
+
+                appendSample(sample);
+
+                if(isSectionEnd(sample))
+                    _processSectionEnd(sample);
+            }
+
+            return morph.buildReadOnly();
+        }
+
+    /**
+       - Append last point of previous section if current section is not a root section
+       - Update the parent ID of the new section
+    **/
+    void _processSectionStart(const Sample& sample)
         {
             bool isRootSection = samples.at(sample.parentId).type == SECTION_SOMA;
 
             if(!isRootSection)
             {
-                properties._points.push_back(samples.at(sample.parentId).point);
-                properties._diameters.push_back(samples.at(sample.parentId).diameter);
-                parentSectionId = swcIdToSectionId.at(sample.parentId);
+                appendSample(samples.at(sample.parentId));
+                currentSectionParentId = swcIdToSectionId.at(sample.parentId);
             } else {
-                parentSectionId = -1;
+                currentSectionParentId = -1;
             }
         }
-        properties._points.push_back(sample.point);
-        properties._diameters.push_back(sample.diameter);
 
-        // End of soma or end of section
-        if(sample.id == lastSomaPoint || // End of soma
-           children[sample.id].size() == 0 || // Reached leaf
-           children[sample.id].size() == 2){ // Reached bifurcation
+/**
+   1) Register in mut::Morphology the Soma/Section with the property object
+   2) Reset the property object
+**/
+    void _processSectionEnd(const Sample& sample)
+        {
             if(sample.id == lastSomaPoint){
                 morph.soma() = std::make_shared<mut::Soma>(mut::Soma(properties));
-            } else { // Section ends here
-                swcIdToSectionId[sample.id] = morph.appendSection(parentSectionId,
+            } else {
+                swcIdToSectionId[sample.id] = morph.appendSection(currentSectionParentId,
                                                                   sample.type,
                                                                   properties);
             }
             properties = Property::PointLevel();
         }
-    }
 
-    return morph.buildReadOnly();
-}
+private:
+    // Dictionnary: SWC Id of the last point of a section to morphio::mut::Section ID
+    std::map<uint32_t, uint32_t> swcIdToSectionId;
+    int lastSomaPoint = -1;
+    std::map<uint32_t, std::vector<uint32_t>> children;
+    std::map<uint32_t, Sample> samples;
+    Property::PointLevel properties;
+    int currentSectionParentId = -1;
+    mut::Morphology morph;
+
+};
 
 Property::Properties load(const URI& uri)
 {
-    // Parsing SWC according to this specification:
-    // http://www.neuronland.org/NLMorphologyConverter/MorphologyFormats/SWC/Spec.html
 
-    return _buildProperties(_readSamples(uri));
+    return SWCBuilder(_readSamples(uri))._buildProperties();
 }
 
 } // namespace swc
