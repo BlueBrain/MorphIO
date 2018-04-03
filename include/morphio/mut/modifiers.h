b2a6e101eb28417491487400cfc6a6bb9ea9b9da
Benoit Coste <benoit.coste@epfl.ch>
2018-04-03 13:33:30 +0200

    Add forgotten modifiers.h

---
+++ include/morphio/mut/modifiers.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <morphio/types.h>
+
+namespace morphio
+{
+namespace mut
+{
+
+namespace modifiers
+{
+
+
+
+/**
+   Only the first and last points of each sections are kept
+**/
+void two_points_sections(morphio::mut::Morphology& morpho);
+
+
+/**
+   Remove duplicated points
+**/
+void no_duplicate_point(morphio::mut::Morphology& morpho);
+
+
+/**
+   Reduce the soma to a sphere placed at the center of gravity of soma points
+   and whose radius is the averaged distance between the soma points and the center of gravity
+**/
+void soma_sphere(morphio::mut::Morphology& morpho);
+
+} // namespace modifiers
+
+} // namespace mut
+
+} // namespace morphio
