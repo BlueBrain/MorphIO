f50f76f925801e5b4e0e9b7a90921772603a341f
Benoit Coste <benoit.coste@epfl.ch>
2018-05-17 14:36:42 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='morphio',
-    version='0.9.3',
+    version='0.9.4',
     author='EPFL - Blue Brain Project',
     author_email='bbp-ou-nse@groupes.epfl.ch',
     description='A hybrid Python/C++ test project',
