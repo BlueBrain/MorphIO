684ad09d8b415cf54eef4d0be9e69896e8c8e059
Benoit Coste <benoit.coste@epfl.ch>
2018-06-11 17:33:51 +0200

    Do not build the docker image when it already exist

--- packaging/python_wheel/build_docker_image.sh
+++ packaging/python_wheel/build_docker_image.sh
@@ -2,10 +2,16 @@
 set -o xtrace
 BASE=$(git rev-parse --show-toplevel)
 
-docker build --no-cache -t morphio_wheel \
-    --build-arg http_proxy=${HTTP_PROXY-$http_proxy} \
-    --build-arg https_proxy=${HTTPS_PROXY-$https_proxy} \
-    $BASE/packaging/python_wheel | tee build.log
+if [ "$(docker images | grep morphio_wheel | wc -l)" != "0" ]; then
+    echo "Image already exist. Not rebuilding it."
 
-ID=$(tail -1 build.log | awk '{print $3;}')
-upload2repo -t docker -i $ID -g latest -n morphio_wheel
+else
+    echo "docker image morphio_wheel not found, building it..."
+    docker build -t morphio_wheel \
+           --build-arg http_proxy=${HTTP_PROXY-$http_proxy} \
+           --build-arg https_proxy=${HTTPS_PROXY-$https_proxy} \
+           $BASE/packaging/python_wheel | tee build.log
+
+    ID=$(tail -1 build.log | awk '{print $3;}')
+    upload2repo -t docker -i $ID -g latest -n morphio_wheel
+fi
