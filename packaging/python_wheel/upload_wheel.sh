9109c599b4e56c5d3bc31f0b89850fdbcd3f397a
Benoit Coste <benoit.coste@protonmail.com>
2018-12-17 11:00:13 +0100

    Bump to version 2.0.3 (#35)
    And re-enable wheels

--- packaging/python_wheel/upload_wheel.sh
+++ packaging/python_wheel/upload_wheel.sh
@@ -8,9 +8,9 @@ else
     CHANNEL=dev
 fi
 
-# for wheel in `ls $BASE/packaging/python_wheel/wheelhouse/morphio*.whl`; do
-#     upload2repo -t python -r $CHANNEL -f $wheel
-# done
+for wheel in `ls $BASE/packaging/python_wheel/wheelhouse/morphio*.whl`; do
+    upload2repo -t python -r $CHANNEL -f $wheel
+done
 
 upload2repo -t python -r $CHANNEL -f $BASE/dist/MorphIO-*.tar.gz
 
