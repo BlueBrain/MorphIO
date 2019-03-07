bd1266ad176246e969d432f66c26f362dec0c9c1
Benoît Coste <benoit.coste@epfl.ch>
2019-03-07 14:49:27 +0100

    Test multibuild

---
+++ config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+# Define custom utilities
+# Test for macOS with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    :
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    pip install nose
+    nosetests /io
+}
