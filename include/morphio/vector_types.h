6c898715904de98efa6a42b3e8f102bb27c1f46e
Matthias Wolf <matthias.wolf@epfl.ch>
2019-08-08 12:30:36 +0200

    Add range instantiation.

--- include/morphio/vector_types.h
+++ include/morphio/vector_types.h
@@ -33,6 +33,7 @@ template <typename T>
 float maxDistanceToCenterOfGravity(const T& points);
 
 extern template const Point centerOfGravity(const std::vector<std::array<float, 3>>&);
+extern template float maxDistanceToCenterOfGravity(const range<const Point>&);
 extern template float maxDistanceToCenterOfGravity(const std::vector<std::array<float, 3>>&);
 
 std::string dumpPoint(const Point& point);
