ddae6c8c285224d909a55773badfeda5e4862cba
Matthias Wolf <matthias.wolf@epfl.ch>
2019-09-12 10:51:40 +0200

    versioning: rely on git. (#102)

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ class CMakeBuild(build_ext):
         extdir = os.path.abspath(
             os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DMORPHIO_VERSION_STRING=' + self.distribution.get_version(),
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
         cfg = 'Debug' if self.debug else 'Release'
