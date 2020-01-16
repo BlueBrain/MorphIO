c5dde964dff2b16384e232ae8b85a3def40fda1f
Benoit Coste <benoit.coste@protonmail.com>
2020-01-16 16:26:35 +0100

    Fix sort stability when using Option.nrn_order (#144)

--- tests/test_7_modifiers.py
+++ tests/test_7_modifiers.py
@@ -18,26 +18,45 @@ def test_no_modifier():
     assert_array_equal(SIMPLE_NO_MODIFIER.points, Morphology(SIMPLE, options=Option.no_modifier).points)
 
 def test_nrn_order():
-    m = Morphology(os.path.join(_path, 'reversed_NRN_neurite_order.swc'), options=Option.nrn_order)
+    filename = os.path.join(_path, 'reversed_NRN_neurite_order.swc')
+
+    m = Morphology(filename, options=Option.nrn_order)
     assert_equal([section.type for section in m.root_sections],
                  [SectionType.axon,
                   SectionType.basal_dendrite,
                   SectionType.apical_dendrite])
 
-
-    normal = Morphology(os.path.join(_path, 'reversed_NRN_neurite_order.swc'))
+    normal = Morphology(filename)
     m = MutableMorphology(normal, options=Option.nrn_order)
     assert_equal([section.type for section in m.root_sections],
                  [SectionType.axon,
                   SectionType.basal_dendrite,
                   SectionType.apical_dendrite])
 
-    normal = MutableMorphology(os.path.join(_path, 'reversed_NRN_neurite_order.swc'))
+    normal = MutableMorphology(filename)
     m = MutableMorphology(normal, options=Option.nrn_order)
     assert_equal([section.type for section in m.root_sections],
                  [SectionType.axon,
                   SectionType.basal_dendrite,
                   SectionType.apical_dendrite])
+
+
+def test_nrn_order_stability():
+    m = Morphology(os.path.join(_path, 'mono-type.asc'), Option.nrn_order)
+    assert_array_equal([root.points[0, 0] for root in m.root_sections
+                        if root.type == SectionType.basal_dendrite], [0, 1, 2, 3])
+    assert_array_equal([root.points[0, 0] for root in m.root_sections
+                        if root.type == SectionType.axon], [0, 1])
+    assert_array_equal([root.points[0, 0] for root in m.root_sections
+                        if root.type == SectionType.apical_dendrite], [1, 0])
+
+    # This morphology used to give a different order when opened with nrn_order flag
+    filename = os.path.join(_path, 'nrn-order-already-sorted.h5')
+    a = Morphology(filename, Option.nrn_order)
+    b = Morphology(filename)
+    for sec1, sec2 in zip(a.root_sections, b.root_sections):
+        assert_array_equal(sec1.points, sec2.points)
+
 
 
 def test_two_point_section():
