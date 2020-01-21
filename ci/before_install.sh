d0f4f8c8ae258ae65926b7f51f6e5867255cfd3c
Benoît Coste <benoit.coste@epfl.ch>
2020-01-21 16:40:46 +0100

    Remove script file added by mistake

--- ci/before_install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# This script is run in the 'before_install' step of .travis.yaml
-if [[ $TRAVIS_OS_NAME = linux ]]; then
-    sudo apt-get install libhdf5-dev
-else
-    brew list hdf5 || brew install hdf5
-fi
