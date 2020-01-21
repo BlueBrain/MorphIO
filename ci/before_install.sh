e4ddf84a4b0764b4222ecae044751f64149c1dd9
Benoît Coste <benoit.coste@epfl.ch>
2020-01-21 13:20:06 +0100

    fixup! Fix .travis.yaml

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -2,7 +2,7 @@
 
 # This script is run in the 'before_install' step of .travis.yaml
 if [[ $TRAVIS_OS_NAME = linux ]]; then
-    sudo apt-get install libhdf5-dev;
+    sudo apt-get install libhdf5-dev
 else
-    brew list hdf5 || brew install hdf5;
+    brew list hdf5 || brew install hdf5
 fi
