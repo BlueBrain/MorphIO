0babd60354a5307e8f26362eb47f836f2262c073
Benoit Coste <benoit.coste@epfl.ch>
2020-02-07 16:22:30 +0100

    gogo

--- ci/get_hdf5.py
+++ ci/get_hdf5.py
@@ -139,9 +139,9 @@ def main():
     if install_path is not None:
         if not exists(install_path):
             makedirs(install_path)
+    cmake_generator = 'Ninja'
     if vs_version is not None:
         # cmake_generator = VSVERSION_TO_GENERATOR[vs_version]
-        cmake_generator = 'Ninja'
         if vs_version == '9-64':
             # Needed for
             # http://help.appveyor.com/discussions/kb/38-visual-studio-2008-64-bit-builds
