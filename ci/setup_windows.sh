5c0514516d9825e55b6f49c43dfce63c8a5613be
Benoît Coste <benoit.coste@epfl.ch>
2020-03-27 11:25:06 +0100

    Simplify windows setup

--- ci/setup_windows.sh
+++
@@ -1,6 +0,0 @@
-choco install --yes miniconda3
-source C:/Tools/miniconda3/Scripts/activate ;
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda install -c conda-forge xtl xsimd xtensor
-conda install -c conda-forge boost-cpp hdf5 eigen;
