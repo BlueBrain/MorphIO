e7b6e2c7c6730df60e61d744baef8921f1756eab
Benoit Coste <benoit.coste@protonmail.com>
2020-05-28 03:16:08 -0700

    Do not keep the h5py to top level (#188)

--- tests/test_6_writers.py
+++ tests/test_6_writers.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 from numpy.testing import assert_array_equal, assert_equal, assert_raises
 from nose.tools import ok_
-import h5py
 from pathlib2 import Path
 
 from morphio.mut import Morphology
@@ -75,6 +74,7 @@ def test_write_basic():
         assert_array_equal(ImmutMorphology(os.path.join(tmp_folder, "test_write.swc")).points, expected)
         assert_array_equal(ImmutMorphology(os.path.join(tmp_folder, "test_write.h5")).points, expected)
 
+        import h5py
         with h5py.File(h5_out, 'r') as h5_file:
             ok_('/perimeters' not in h5_file.keys())
 
