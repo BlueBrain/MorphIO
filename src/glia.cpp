6ffc32492b1176689b1c8ce8dc17055b0c9a54bc
Benoît Coste <benoit.coste@epfl.ch>
2020-11-12 13:57:23 +0100

    format

--- src/glia.cpp
+++ src/glia.cpp
@@ -4,7 +4,8 @@ namespace morphio {
 
 Glia::Glia(const std::string& source) : Morphology(source) {
     if (_properties->_cellLevel._cellFamily != CellFamily::GLIA)
-        throw(RawDataError("File: " + source + " is not a Glia file. It should be a H5 file the cell type GLIA."));
+        throw(RawDataError("File: " + source +
+                           " is not a Glia file. It should be a H5 file the cell type GLIA."));
 }
 
 }  // namespace morphio
--- src/mut/glia.cpp
+++ src/mut/glia.cpp
@@ -9,7 +9,8 @@ Glia::Glia() : Morphology() {
 
 Glia::Glia(const std::string& source) : Morphology(source) {
     if (_cellProperties->_cellFamily != CellFamily::GLIA)
-        throw(RawDataError("File: " + source + " is not a Glia file. It should be a H5 file the cell type GLIA."));
+        throw(RawDataError("File: " + source +
+                           " is not a Glia file. It should be a H5 file the cell type GLIA."));
 }
 
 }  // namespace mut
