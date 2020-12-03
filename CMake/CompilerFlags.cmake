cb9b58e300fcad333c549dabb96d51cae4bd3099
Benoît Coste <benoit.coste@epfl.ch>
2020-12-03 14:43:18 +0100

    no wx

--- CMake/CompilerFlags.cmake
+++ CMake/CompilerFlags.cmake
@@ -1,2 +1,2 @@
 # Taken from https://github.com/BlueBrain/hpc-coding-conventions/blob/master/cpp/cmake/bob.cmake#L192-L255
-set(FLAGS "${FLAGS} /w /DH5_BUILT_AS_DYNAMIC_LIB /EHsc /D_USE_MATH_DEFINES")
+set(FLAGS "${FLAGS} /w /WX:NO /DH5_BUILT_AS_DYNAMIC_LIB /EHsc /D_USE_MATH_DEFINES")
