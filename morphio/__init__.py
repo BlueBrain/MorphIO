c3f0cefd03d837fc475e75aa1f6ee84b8958f9d2
Mike Gevaert <michael.gevaert@epfl.ch>
2021-02-25 08:28:33 +0100

    windows build

--- morphio/__init__.py
+++ morphio/__init__.py
@@ -1,3 +1,15 @@
+import platform
+
+
+if platform.system() == "Windows":
+    # the plan is to leverage the h5py inclusion of hdf5 dll; this way
+    # there cannot be a mismatch between version, when using windows,
+    # and MorphIO doesn't need to build hdf5
+    import h5py as _h5py
+    assert (1, 12) == _h5py.version.hdf5_version_tuple[:2], f'1.12.x != {_h5py.version.hdf5_version}'
+    del _h5py
+
+
 from ._morphio import (
     Annotation,
     AnnotationType,
--- setup.py
+++ setup.py
@@ -13,39 +13,10 @@ from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
 
-MIN_CPU_CORES = 2
-
-
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
-
-
-def find_cmake():
-    for candidate in ['cmake', 'cmake3']:
-        try:
-            out = subprocess.check_output([candidate, '--version'])
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
-                                                   out.decode()).group(1))
-            if cmake_version >= '3.2.0':
-                return candidate
-        except OSError:
-            pass
-
-    raise RuntimeError("CMake >= 3.2.0 must be installed to build MorphIO")
-
-
-def get_cpu_count():
-    try:
-        return len(os.sched_getaffinity(0))  # linux only
-    except:
-        pass
-
-    try:
-        return os.cpu_count()  # python 3.4+
-    except:
-        return 1  # default
 
 
 class CMakeBuild(build_ext):
@@ -58,11 +29,10 @@ class CMakeBuild(build_ext):
         self.cmake_defs = None
 
     def run(self):
-        cmake = find_cmake()
         for ext in self.extensions:
-            self.build_extension(ext, cmake)
+            self.build_extension(ext)
 
-    def build_extension(self, ext, cmake):
+    def build_extension(self, ext):
         extdir = os.path.abspath(
             os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
@@ -79,35 +49,41 @@ class CMakeBuild(build_ext):
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
-        if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
-                cfg.upper(),
-                extdir)]
-            cmake_args += ['-G', os.getenv('MORPHIO_CMAKE_GENERATOR',
-                                           "Visual Studio 15 2017 Win64")]
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE={}'.format(cfg),
-                           '-DMorphIO_CXX_WARNINGS=OFF',
-                           ]
-            build_args += ["--", "-j{}".format(max(MIN_CPU_CORES, get_cpu_count())),
-                           ]
+        #if platform.system() == "Windows":
+        #    cmake_args += ['-G', os.getenv('MORPHIO_CMAKE_GENERATOR', 'Ninja'),
+        #                   ]
+
+        cmake_args += ['-DCMAKE_BUILD_TYPE={}'.format(cfg),
+                       '-DMorphIO_CXX_WARNINGS=OFF',
+                       '-GNinja',
+                       ]
 
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
-        subprocess.check_call([cmake, ext.sourcedir] + cmake_args, cwd=self.build_temp)
-        subprocess.check_call([cmake, '--build', '.', '--target', '_morphio'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.', '--target', '_morphio'] + build_args, cwd=self.build_temp)
 
-with open('README.rst') as f:
+#XXX
+long_description = 'f.read()'
+
+install_requires = ['numpy>=1.14.1',
+                    ]
+
+with open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
+
+if platform.system() == 'Windows':
+    install_requires += ['h5py',   # use h5py's hdf5 install
+                         ]
 
 setup(
     name='MorphIO',
     author='Blue Brain Project, EPFL',
     description='A neuron morphology IO library',
     long_description=long_description,
-    long_description_content_type='text/x-rst',
-    install_requires=['numpy>=1.14.1'],
+    long_description_content_type="text/markdown",
+    install_requires=install_requires,
     extras_require={
         'docs': ['sphinx-bluebrain-theme'],
     },
@@ -126,5 +102,6 @@ setup(
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
     ],
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=('setuptools_scm',
+                    )
 )
