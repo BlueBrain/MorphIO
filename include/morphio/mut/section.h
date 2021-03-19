14214deea3d106c3a730e4344c8a8dad67c22e04
Jean Jacquemier <jacquemier@epfl.ch>
2021-03-19 16:23:37 +0100

    Clang format fix

--- include/morphio/mut/section.h
+++ include/morphio/mut/section.h
@@ -92,8 +92,7 @@ class Section: public std::enable_shared_from_this<Section>
     upstream_iterator upstream_end() const;
 
 
-    std::shared_ptr<Section> appendSection(const morphio::NeuronalSection&,
-                                           bool recursive = false);
+    std::shared_ptr<Section> appendSection(const morphio::NeuronalSection&, bool recursive = false);
     std::shared_ptr<Section> appendSection(std::shared_ptr<Section> original_section,
                                            bool recursive = false);
 
