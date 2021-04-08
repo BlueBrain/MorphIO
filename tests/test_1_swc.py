2a19dfcf489505946e7609b227d393f823dfcbc5
aleksei sanin <aleksei.sanin@epfl.ch>
2021-04-08 11:13:14 +0200

    Revert "warn about 2-point swc soma (#270)" due to https://github.com/neuronsimulator/nrn/pull/1144
    This reverts commit 24d7c1d1

--- tests/test_1_swc.py
+++ tests/test_1_swc.py
@@ -244,19 +244,10 @@ def test_soma_type():
                      SomaType.SOMA_SINGLE_POINT)
 
     # 2 point soma
-    with captured_output() as (_, err):
-        with ostream_redirect(stdout=True, stderr=True):
-            with tmp_swc_file('''1 1 0 0 0 3.0 -1
-                                 2 1 0 0 0 3.0  1''') as tmp_file:
-                assert_equal(Morphology(tmp_file.name).soma_type,
-                             SomaType.SOMA_UNDEFINED)
-                assert_string_equal(
-                    '''{}:0:warning
-                       Warning: SWC soma has 2 points. Implicitly 3-points and 1-point SWC somas
-                       are cylinders. 2-points SWC soma has no implicit well-established meaning.
-                       0 0 0
-                       0 0 0
-                    '''.format(tmp_file.name), err.getvalue())
+    with tmp_swc_file('''1 1 0 0 0 3.0 -1
+                         2 1 0 0 0 3.0  1''') as tmp_file:
+        assert_equal(Morphology(tmp_file.name).soma_type,
+                     SomaType.SOMA_UNDEFINED)
 
     # > 3 points soma
     with tmp_swc_file('''1 1 0 0 0 3.0 -1
