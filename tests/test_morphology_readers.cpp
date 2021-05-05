8e2a38f05b59937f11a29d5c0856a153af0bbc3d
MikeG <michael.gevaert@epfl.ch>
2021-05-05 17:04:26 +0200

    cleanup morphologyHDF5 (#285)
    * make which datasets belong to which versions more clear
    * don't use try/catch to detect if datasets exist
    * try and clarify where member variables are read & saved
    * removed member variables _points, _pointsDims, _sections, since their locality can be kept local
    * fix version numbering in tests/data/h5/v1/endoplasmic-reticulum.h5 / mitochondria.h5
    * Fix the no soma h5 morphologies (#288)
    * Add tests for somatypes
    * test for extra somas/out of order during traversal of sections
    * 100 % coverage for src/readers/morphologyHDF5.cpp
    * added explicit glia tests
    * variable rename, use static_cast
    
    Co-authored-by: tomdele <thomas.delemontex@epfl.ch>

--- tests/test_morphology_readers.cpp
+++ tests/test_morphology_readers.cpp
@@ -4,12 +4,128 @@
 #include <highfive/H5File.hpp>
 #include <morphio/morphology.h>
 #include <morphio/mut/morphology.h>
+#include <morphio/soma.h>
 
+namespace {
+bool almost_equal(morphio::floatType a, double expected, double epsilon) {
+#ifdef MORPHIO_USE_DOUBLE
+    return std::abs(a - expected) < epsilon;
+#else
+    return std::abs(static_cast<double>(a) - expected) < epsilon;
+#endif
+}
+}  // namespace
 
 TEST_CASE("LoadH5Morphology", "[morphology]") {
-    const morphio::Morphology m("data/h5/v1/Neuron.h5");
+    {
+        const morphio::Morphology m("data/h5/v1/Neuron.h5");
+        REQUIRE(m.soma().points().size() == 3);
+        REQUIRE(m.diameters().size() == 924);
+        REQUIRE(m.points().size() == 924);
+        // 3 point soma
+        REQUIRE(m.somaType() == morphio::SomaType::SOMA_SIMPLE_CONTOUR);
+    }
 
-    REQUIRE(m.diameters().size() == 924);
+    {
+        const morphio::Morphology m("data/h5/v1/simple.h5");
+        REQUIRE(m.soma().points().size() == 1);
+        REQUIRE(m.diameters().size() == 12);
+        REQUIRE(m.points().size() == 12);
+        // 1 point soma
+        REQUIRE(m.somaType() == morphio::SomaType::SOMA_SINGLE_POINT);
+    }
+
+    {
+        const morphio::Morphology m("data/h5/v1/simple-two-point-soma.h5");
+        REQUIRE(m.soma().points().size() == 2);
+        REQUIRE(m.diameters().size() == 12);
+        REQUIRE(m.points().size() == 12);
+        // 2 point soma
+        REQUIRE(m.somaType() == morphio::SomaType::SOMA_UNDEFINED);
+    }
+
+    {  // file is an not a valid h5 file
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/non-valid.h5"), morphio::RawDataError);
+    }
+
+    {  // h5v2 is not supported
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/v2/Neuron.h5"), morphio::RawDataError);
+    }
+
+    {  // empty h5 file doesn't have /points or /structure
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/empty.h5"), morphio::RawDataError);
+    }
+
+    {  // empty metadata group
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/v1/metadata_group_only.h5"),
+                        morphio::RawDataError);
+    }
+
+    {  // unsupported version number
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/v1/h5v1.3.h5"), morphio::RawDataError);
+    }
+
+    {  // incorrect points shape
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/v1/incorrect_point_columns.h5"),
+                        morphio::RawDataError);
+    }
+
+    {  // incorrect structure shape
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/v1/incorrect_structure_columns.h5"),
+                        morphio::RawDataError);
+    }
+
+    {  // incorrect soma section structure
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/v1/three-point-soma-two-offset.h5"),
+                        morphio::RawDataError);
+    }
+
+    {  // incorrect type in /structure
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/v1/unknown_section_type_structure.h5"),
+                        morphio::RawDataError);
+    }
+
+    {  // soma after dendrite
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/v1/soma_after_dendrite.h5"),
+                        morphio::RawDataError);
+    }
+}
+
+
+TEST_CASE("LoadH5Glia", "[morphology]") {
+    {
+        const morphio::Morphology m("data/h5/v1/glia.h5");
+        REQUIRE(m.soma().points().size() == 2);
+        REQUIRE(m.points().size() == 2);
+        REQUIRE(m.perimeters().size() == 2);
+    }
+
+    {
+        const morphio::Morphology m("data/h5/v1/glia_soma_only.h5");
+        REQUIRE(m.soma().points().size() == 1);
+        REQUIRE(m.points().size() == 0);
+        REQUIRE(m.perimeters().size() == 0);
+    }
+
+    {  // empty perimeters
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/v1/glia_empty_perimeters.h5"),
+                        morphio::RawDataError);
+    }
+
+    {  // wrong sized perimeters
+        CHECK_THROWS_AS(morphio::Morphology("data/h5/v1/glia_wrong_sized_perimeters.h5"),
+                        morphio::RawDataError);
+    }
+}
+
+TEST_CASE("LoadH5MorphologySingleNeurite", "[morphology]") {
+    {
+        const morphio::Morphology m("data/h5/v1/single-neurite.h5");
+        REQUIRE(m.soma().points().empty());
+        REQUIRE(m.points().size() == 3);
+        REQUIRE(almost_equal(m.points()[0][0], 4., 0.001));
+        REQUIRE(almost_equal(0., 0., 0.1));
+    }
 }
 
 TEST_CASE("LoadSWCMorphology", "[morphology]") {
