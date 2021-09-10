3821676fc7622908be675becab0ab5f62202ccff
Mike Gevaert <michael.gevaert@epfl.ch>
2021-09-10 14:25:17 +0200

    final test

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ with open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 if platform.system() == 'Windows':
-    install_requires += ['h5py',   # use h5py's hdf5 install
+    install_requires += ['h5py>=3,<4',   # use h5py's hdf5 install so we don't have to redistrute hdf5
                          ]
 
 setup(
