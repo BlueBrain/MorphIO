09e78de732d3e5fdea3db54cf322ff3cb9a9c5ce
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2021-10-13 15:49:11 +0200

    Add cpp test for section offsets

---
+++ tests/test_vasculature_morphology.cpp
@@ -0,0 +1,42 @@
+#include "contrib/catch.hpp"
+#include <morphio/vasc/section.h>
+#include <morphio/vasc/vasculature.h>
+
+
+using Vasculature = morphio::vasculature::Vasculature;
+
+namespace {
+
+struct Files
+{
+    std::string vasculature;
+
+    Files(): vasculature("data/h5/vasculature1.h5") {}
+};
+
+}  // anonymous namespace
+
+
+TEST_CASE("vasculature_section_offsets", "[vasculature]"){
+
+    Files files;
+    morphio::vasculature::Vasculature morph(files.vasculature);
+
+    const auto& sections = morph.sections();
+    size_t n_offsets = sections.size() + 1;
+
+    std::vector<uint32_t> expected_section_offsets;
+    expected_section_offsets.reserve(n_offsets);
+
+    uint32_t offset = 0;
+    expected_section_offsets.push_back(offset);
+
+    for (const auto& section: sections){
+
+        offset += static_cast<uint32_t>(section.points().size());
+        expected_section_offsets.push_back(offset);
+    }
+
+    REQUIRE(morph.sectionOffsets() == expected_section_offsets);
+
+}
