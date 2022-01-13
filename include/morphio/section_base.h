a4873a928190d96e1780d45b06d948f5d6e42e54
Mike Gevaert <michael.gevaert@epfl.ch>
2022-01-13 09:52:30 +0100

    remove section_base.tpp

--- include/morphio/section_base.h
+++ include/morphio/section_base.h
@@ -25,12 +25,6 @@ template <typename T>
 class SectionBase
 {
   public:
-    SectionBase() = default;
-
-    SectionBase(const SectionBase& section);
-
-    SectionBase& operator=(const SectionBase& other);
-
     inline bool operator==(const SectionBase& other) const noexcept;
     inline bool operator!=(const SectionBase& other) const noexcept;
 
@@ -80,6 +74,70 @@ inline uint32_t SectionBase<T>::id() const noexcept {
     return _id;
 }
 
-}  // namespace morphio
+template <typename T>
+SectionBase<T>::SectionBase(const uint32_t id_,
+                            const std::shared_ptr<Property::Properties>& properties)
+    : _id(id_)
+    , _properties(properties) {
+    const auto& sections = properties->get<typename T::SectionId>();
+    if (_id >= sections.size())
+        throw RawDataError(
+            "Requested section ID (" + std::to_string(_id) +
+            ") is out of array bounds (array size = " + std::to_string(sections.size()) + ")");
 
-#include "section_base.tpp"
+    const auto start = static_cast<size_t>(sections[_id][0]);
+    const size_t end = _id == sections.size() - 1
+                           ? properties->get<typename T::PointAttribute>().size()
+                           : static_cast<size_t>(sections[_id + 1][0]);
+
+    _range = std::make_pair(start, end);
+
+    if (_range.second <= _range.first)
+        std::cerr << "Dereferencing broken properties section " << _id
+                  << "\nSection range: " << _range.first << " -> " << _range.second << '\n';
+}
+
+template <typename T>
+template <typename TProperty>
+range<const typename TProperty::Type> SectionBase<T>::get() const {
+    const auto& data = _properties->get<TProperty>();
+    if (data.empty())
+        return {};
+
+    auto ptr_start = data.data() + _range.first;
+    return {ptr_start, _range.second - _range.first};
+}
+
+template <typename T>
+bool SectionBase<T>::isRoot() const {
+    return _properties->get<typename T::SectionId>()[_id][1] == -1;
+}
+
+template <typename T>
+T SectionBase<T>::parent() const {
+    if (isRoot())
+        throw MissingParentError("Cannot call Section::parent() on a root node (section id=" +
+                                 std::to_string(_id) + ").");
+
+    const auto _parent = static_cast<unsigned int>(
+        _properties->get<typename T::SectionId>()[_id][1]);
+    return {_parent, _properties};
+}
+
+template <typename T>
+std::vector<T> SectionBase<T>::children() const {
+    std::vector<T> result;
+    try {
+        const std::vector<uint32_t>& _children = _properties->children<typename T::SectionId>().at(
+            static_cast<int>(_id));
+        result.reserve(_children.size());
+        for (const uint32_t id_ : _children)
+            result.push_back(T(id_, _properties));
+
+        return result;
+    } catch (const std::out_of_range&) {
+        return result;
+    }
+}
+
+}  // namespace morphio
