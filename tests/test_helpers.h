889d774d32d0b34bb9650463049ae63c8277c215
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2022-03-25 14:13:13 +0100

    Update gsl-lite submodule to v0.40.0

--- tests/test_helpers.h
+++ tests/test_helpers.h
@@ -1,26 +1,26 @@
 #include <iostream>
 
-static bool almost_equal(morphio::floatType a, double expected, double epsilon) {
+static bool almost_equal(morphio::floatType actual, double expected, double epsilon) {
 #ifdef MORPHIO_USE_DOUBLE
-    bool res = std::abs(a - expected) < epsilon;
+    bool res = std::abs(actual - expected) < epsilon;
 #else
-    bool res = std::abs(static_cast<double>(a) - expected) < epsilon;
+    bool res = std::abs(static_cast<double>(actual) - expected) < epsilon;
 #endif
     if (!res) {
-        std::cerr << "Failed almost equal: " << a << " != " << expected
+        std::cerr << "Failed almost equal: " << actual << " != " << expected
                   << " (expected) with epsilon of " << epsilon << '\n';
     }
     return res;
 }
 
-static bool array_almost_equal(const std::vector<morphio::floatType>& a,
+static bool array_almost_equal(const std::vector<morphio::floatType>& actual,
                                const std::vector<double>& expected,
                                double epsilon) {
-    if (a.size() != expected.size()) {
+    if (actual.size() != expected.size()) {
         return false;
     }
-    for (size_t i = 0; i < a.size(); i++) {
-        if (!almost_equal(a.at(i), expected.at(i), epsilon)) {
+    for (size_t i = 0; i < actual.size(); i++) {
+        if (!almost_equal(actual[i], expected[i], epsilon)) {
             return false;
         }
     }
