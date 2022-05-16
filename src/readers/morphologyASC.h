77653199dc9492023b4e054ac49d73bc4254b2d9
Mike Gevaert <michael.gevaert@epfl.ch>
2022-05-16 15:49:08 +0200

    cleanup ASC reader

--- src/readers/morphologyASC.h
+++ src/readers/morphologyASC.h
@@ -4,7 +4,7 @@
 namespace morphio {
 namespace readers {
 namespace asc {
-Property::Properties load(const std::string& uri, unsigned int options);
+Property::Properties load(const std::string& path, unsigned int options);
 }  // namespace asc
 }  // namespace readers
 }  // namespace morphio
