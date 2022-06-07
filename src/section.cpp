f3f45b7e351edfc7b6a31ee8b09232edb48fe716
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2022-06-07 15:40:03 +0200

    Move single lines from readonly section source to header

--- src/section.cpp
+++ src/section.cpp
@@ -17,46 +17,6 @@ bool Section::isHeterogeneous(bool downstream) const {
     return std::any_of(upstream_begin(), upstream_end(), predicate);
 }
 
-SectionType Section::type() const {
-    return properties_->get<Property::SectionType>()[id_];
-}
-
-depth_iterator Section::depth_begin() const {
-    return depth_iterator(*this);
-}
-
-depth_iterator Section::depth_end() const {
-    return depth_iterator();
-}
-
-breadth_iterator Section::breadth_begin() const {
-    return breadth_iterator(*this);
-}
-
-breadth_iterator Section::breadth_end() const {
-    return breadth_iterator();
-}
-
-upstream_iterator Section::upstream_begin() const {
-    return upstream_iterator(*this);
-}
-
-upstream_iterator Section::upstream_end() const {
-    return upstream_iterator();
-}
-
-range<const Point> Section::points() const {
-    return get<Property::Point>();
-}
-
-range<const floatType> Section::diameters() const {
-    return get<Property::Diameter>();
-}
-
-range<const floatType> Section::perimeters() const {
-    return get<Property::Perimeter>();
-}
-
 bool Section::hasSameShape(const Section& other) const noexcept {
     return (other.type() == type() && other.diameters() == diameters() &&
             other.points() == points() && other.perimeters() == perimeters());
