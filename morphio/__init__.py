5438b805dd433cdf953cf583f2ca710b2763c9a4
Robin De Schepper <robin.deschepper93@gmail.com>
2023-01-03 09:08:41 +0100

    Replace assert by exception (#436)

--- morphio/__init__.py
+++ morphio/__init__.py
@@ -6,7 +6,8 @@ if platform.system() == "Windows":
     # there cannot be a mismatch between version, when using windows,
     # and MorphIO doesn't need to build hdf5
     import h5py as _h5py
-    assert (1, 12) == _h5py.version.hdf5_version_tuple[:2], f'1.12.x != {_h5py.version.hdf5_version}'
+    if (1, 12) != _h5py.version.hdf5_version_tuple[:2]:
+        raise RuntimeError(f'HDF5 library version mismatch. 1.12.x != {_h5py.version.hdf5_version}')
     del _h5py
 
 
