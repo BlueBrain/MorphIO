d4b026e7e65cd8e702afd43d83334407f0af4bc2
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-05-31 14:24:47 +0200

    API for optimized access order in Collection (#453)
    The point of this API is to enable collections to load morphologies
    in any order they see fit. For HDF5 containers tests have shown that the
    access pattern matters a lot in terms of performance.
    
    The idea is a generic API that allows optimizing the iteration order for
    reorderable loops, e.g.,
    
        for k, morph_name in enumerate(morphology_names):
            morph = collection.load(morph_name)
            f(k, morph)
    
    can be replaced with
    
        for k, morph in collection.load_unordered(morphology_names):
            assert collection.load(morphology_names[k]) == morph
            f(k, morph)

--- src/collection.cpp
+++ src/collection.cpp
@@ -7,13 +7,83 @@
 
 namespace morphio {
 
+class LoadUnorderedImpl
+{
+  public:
+    virtual ~LoadUnorderedImpl() = default;
+
+    virtual Morphology load(size_t k) const = 0;
+    virtual mut::Morphology load_mut(size_t k) const = 0;
+
+    virtual size_t size() const = 0;
+};
+
+namespace detail {
+
+/**
+ *  Load morphologies in the specified order.
+ *
+ *  Given access to the `collection`, the morphology names and an ordered
+ *  vectors of loop indices, one can implement simple optimized access pattern
+ *  by simply loading the morphologies in the order the loop indices prescribe.
+ *
+ *  The above approach is implemented by this class.
+ */
+class LoadUnorderedFromLoopIndices: public LoadUnorderedImpl
+{
+  public:
+    LoadUnorderedFromLoopIndices(Collection collection,
+                                 std::vector<size_t> loop_indices,
+                                 std::vector<std::string> morphology_names,
+                                 unsigned int options)
+        : _collection(std::move(collection))
+        , _loop_indices(std::move(loop_indices))
+        , _morphology_names(std::move(morphology_names))
+        , _options(options) {}
+
+    size_t size() const override {
+        return _morphology_names.size();
+    }
+
+    Morphology load(size_t k) const override {
+        return load_impl<Morphology>(k);
+    }
+
+    mut::Morphology load_mut(size_t k) const override {
+        return load_impl<mut::Morphology>(k);
+    }
+
+  protected:
+    template <class M>
+    M load_impl(size_t k) const {
+        auto i = _loop_indices[k];
+        return _collection.template load<M>(_morphology_names[i], _options);
+    }
+
+  private:
+    Collection _collection;
+    std::vector<size_t> _loop_indices;
+    std::vector<std::string> _morphology_names;
+    unsigned int _options;
+};
+
+}  // namespace detail
+
+
 class CollectionImpl
 {
   public:
     virtual ~CollectionImpl() = default;
 
     virtual Morphology load(const std::string& morph_name, unsigned int options) const = 0;
+
     virtual mut::Morphology load_mut(const std::string& morph_name, unsigned int options) const = 0;
+
+    virtual std::shared_ptr<LoadUnorderedImpl> load_unordered(Collection collection,
+                                                              std::vector<std::string> morph_name,
+                                                              unsigned int options) const = 0;
+
+    virtual std::vector<size_t> argsort(const std::vector<std::string>& morphology_names) const = 0;
 };
 
 namespace detail {
@@ -21,7 +91,7 @@ template <class Derived>
 class CollectionImpl: public morphio::CollectionImpl
 {
     // The purpose of this class is to implement the two separate
-    // function in terms of a single templated method `load_impl`.
+    // functions in terms of a single templated method `load_impl`.
   public:
     morphio::Morphology load(const std::string& morph_name, unsigned int options) const override {
         const auto& derived = static_cast<const Derived&>(*this);
@@ -32,6 +102,16 @@ class CollectionImpl: public morphio::CollectionImpl
                                       unsigned int options) const override {
         const auto& derived = static_cast<const Derived&>(*this);
         return derived.template load_impl<mut::Morphology>(morph_name, options);
+    }
+
+    std::shared_ptr<LoadUnorderedImpl> load_unordered(Collection collection,
+                                                      std::vector<std::string> morphology_names,
+                                                      unsigned int options) const override {
+        auto loop_indices = argsort(morphology_names);
+        return std::make_shared<detail::LoadUnorderedFromLoopIndices>(std::move(collection),
+                                                                      std::move(loop_indices),
+                                                                      std::move(morphology_names),
+                                                                      options);
     }
 };
 }  // namespace detail
@@ -59,7 +139,17 @@ class DirectoryCollection: public morphio::detail::CollectionImpl<DirectoryColle
             }
         }
 
-        throw MorphioError("Morpholgy '" + morph_name + "' not found in: " + _dirname);
+        throw MorphioError("Morphology '" + morph_name + "' not found in: " + _dirname);
+    }
+
+    std::vector<size_t> argsort(const std::vector<std::string>& morphology_names) const override {
+        auto n_morphologies = morphology_names.size();
+        std::vector<size_t> loop_indices(n_morphologies);
+        for (size_t i = 0; i < n_morphologies; ++i) {
+            loop_indices[i] = i;
+        }
+
+        return loop_indices;
     }
 
   private:
@@ -90,6 +180,31 @@ class HDF5ContainerCollection: public morphio::detail::CollectionImpl<HDF5Contai
 
     HDF5ContainerCollection& operator=(const HDF5ContainerCollection&) = delete;
     HDF5ContainerCollection& operator=(HDF5ContainerCollection&&) = delete;
+
+    std::vector<size_t> argsort(const std::vector<std::string>& morphology_names) const override {
+        auto n_morphologies = morphology_names.size();
+        std::vector<hsize_t> offsets(n_morphologies);
+        std::vector<size_t> loop_indices(n_morphologies);
+
+        for (size_t i = 0; i < n_morphologies; ++i) {
+            loop_indices[i] = i;
+
+            const auto& morph_name = morphology_names[i];
+
+            auto morph = _file.getGroup(morph_name.data());
+            auto points = morph.getDataSet("points");
+
+            auto dcpl = points.getCreatePropertyList();
+            auto layout = H5Pget_layout(dcpl.getId());
+            offsets[i] = layout == H5D_CONTIGUOUS ? points.getOffset() : size_t(-1);
+        }
+
+        std::sort(loop_indices.begin(), loop_indices.end(), [&offsets](size_t i, size_t j) {
+            return offsets[i] < offsets[j];
+        });
+
+        return loop_indices;
+    }
 
   protected:
     friend morphio::detail::CollectionImpl<HDF5ContainerCollection>;
@@ -127,6 +242,7 @@ static std::shared_ptr<morphio::CollectionImpl> open_collection(
 
     throw std::invalid_argument("Invalid path: " + collection_path);
 }
+
 }  // namespace detail
 
 Collection::Collection(std::shared_ptr<CollectionImpl> collection)
@@ -141,8 +257,8 @@ Collection::Collection(std::string collection_path, std::vector<std::string> ext
 
 
 template <class M>
-typename std::enable_if<std::is_same<M, Morphology>::value, M>::type Collection::load(
-    const std::string& morph_name, unsigned int options) const {
+typename enable_if_immutable<M, M>::type Collection::load(const std::string& morph_name,
+                                                          unsigned int options) const {
     if (_collection != nullptr) {
         return _collection->load(morph_name, options);
     }
@@ -151,8 +267,8 @@ typename std::enable_if<std::is_same<M, Morphology>::value, M>::type Collection:
 }
 
 template <class M>
-typename std::enable_if<std::is_same<M, mut::Morphology>::value, M>::type Collection::load(
-    const std::string& morph_name, unsigned int options) const {
+typename enable_if_mutable<M, M>::type Collection::load(const std::string& morph_name,
+                                                        unsigned int options) const {
     if (_collection != nullptr) {
         return _collection->load_mut(morph_name, options);
     }
@@ -160,16 +276,149 @@ typename std::enable_if<std::is_same<M, mut::Morphology>::value, M>::type Collec
     throw std::runtime_error("The collection has been closed.");
 }
 
+std::vector<size_t> Collection::argsort(const std::vector<std::string>& morphology_names) const {
+    if (_collection != nullptr) {
+        return _collection->argsort(morphology_names);
+    }
 
-template typename std::enable_if<std::is_same<mut::Morphology, mut::Morphology>::value,
-                                 mut::Morphology>::type
-Collection::load<mut::Morphology>(const std::string& morph_name, unsigned int options) const;
+    throw std::runtime_error("The collection has been closed.");
+}
 
-template typename std::enable_if<std::is_same<Morphology, Morphology>::value, Morphology>::type
-Collection::load<Morphology>(const std::string& morph_name, unsigned int options) const;
+template mut::Morphology Collection::load<mut::Morphology>(const std::string& morph_name,
+                                                           unsigned int options) const;
+
+template Morphology Collection::load<Morphology>(const std::string& morph_name,
+                                                 unsigned int options) const;
+
+
+template <class M>
+LoadUnordered<M> Collection::load_unordered(std::vector<std::string> morphology_names,
+                                            unsigned int options) const {
+    return LoadUnordered<M>(_collection->load_unordered(*this, morphology_names, options));
+}
+
+template LoadUnordered<mut::Morphology> Collection::load_unordered<mut::Morphology>(
+    std::vector<std::string> morphology_names, unsigned int options) const;
+
+template LoadUnordered<Morphology> Collection::load_unordered<Morphology>(
+    std::vector<std::string> morphology_names, unsigned int options) const;
+
 
 void Collection::close() {
     _collection = nullptr;
 }
+
+
+template <class M>
+typename LoadUnordered<M>::Iterator LoadUnordered<M>::begin() const {
+    return Iterator(_load_unordered_impl, 0);
+}
+
+template <class M>
+typename LoadUnordered<M>::Iterator LoadUnordered<M>::end() const {
+    return Iterator(_load_unordered_impl, _load_unordered_impl->size());
+}
+
+template <class M>
+LoadUnordered<M>::LoadUnordered(std::shared_ptr<LoadUnorderedImpl> load_unordered_impl)
+    : _load_unordered_impl(load_unordered_impl) {}
+
+template <class M>
+LoadUnordered<M>::Iterator::Iterator(std::shared_ptr<LoadUnorderedImpl> load_unordered_impl,
+                                     size_t k)
+    : _k(k)
+    , _load_unordered_impl(std::move(load_unordered_impl)) {}
+
+template <class M>
+bool LoadUnordered<M>::Iterator::operator==(const LoadUnordered::Iterator& other) const {
+    return (*this)._k == other._k;
+}
+
+template <class M>
+bool LoadUnordered<M>::Iterator::operator!=(const LoadUnordered::Iterator& other) const {
+    return !((*this) == other);
+}
+
+template <class M>
+typename LoadUnordered<M>::Iterator& LoadUnordered<M>::Iterator::operator++() {
+    ++_k;
+    return *this;
+}
+
+template <class M>
+typename LoadUnordered<M>::Iterator LoadUnordered<M>::Iterator::operator++(int) {
+    return LoadUnordered<M>::Iterator(_load_unordered_impl, _k++);
+}
+
+template <class M>
+template <class U>
+typename enable_if_mutable<U, std::pair<size_t, M>>::type LoadUnordered<M>::Iterator::operator*()
+    const {
+    return {_k, std::move(_load_unordered_impl->load_mut(_k))};
+}
+
+template <class M>
+template <class U>
+typename enable_if_immutable<U, std::pair<size_t, M>>::type LoadUnordered<M>::Iterator::operator*()
+    const {
+    return {_k, std::move(_load_unordered_impl->load(_k))};
+}
+
+
+template LoadUnordered<Morphology>::Iterator::Iterator(
+    std::shared_ptr<LoadUnorderedImpl> load_unordered_impl, size_t k);
+
+template LoadUnordered<mut::Morphology>::Iterator::Iterator(
+    std::shared_ptr<LoadUnorderedImpl> load_unordered_impl, size_t k);
+
+
+template typename LoadUnordered<Morphology>::Iterator LoadUnordered<Morphology>::begin() const;
+
+template typename LoadUnordered<mut::Morphology>::Iterator LoadUnordered<mut::Morphology>::begin()
+    const;
+
+template typename LoadUnordered<Morphology>::Iterator LoadUnordered<Morphology>::end() const;
+
+template typename LoadUnordered<mut::Morphology>::Iterator LoadUnordered<mut::Morphology>::end()
+    const;
+
+template LoadUnordered<Morphology>::LoadUnordered(
+    std::shared_ptr<LoadUnorderedImpl> load_unordered_impl);
+
+template LoadUnordered<mut::Morphology>::LoadUnordered(
+    std::shared_ptr<LoadUnorderedImpl> load_unordered_impl);
+
+
+template bool LoadUnordered<Morphology>::Iterator::operator==(
+    const LoadUnordered::Iterator& other) const;
+
+template bool LoadUnordered<mut::Morphology>::Iterator::operator==(
+    const LoadUnordered::Iterator& other) const;
+
+
+template bool LoadUnordered<Morphology>::Iterator::operator!=(
+    const LoadUnordered::Iterator& other) const;
+
+template bool LoadUnordered<mut::Morphology>::Iterator::operator!=(
+    const LoadUnordered::Iterator& other) const;
+
+template typename LoadUnordered<Morphology>::Iterator&
+LoadUnordered<Morphology>::Iterator::operator++();
+
+template typename LoadUnordered<mut::Morphology>::Iterator&
+LoadUnordered<mut::Morphology>::Iterator::operator++();
+
+template typename LoadUnordered<Morphology>::Iterator
+LoadUnordered<Morphology>::Iterator::operator++(int);
+
+template typename LoadUnordered<mut::Morphology>::Iterator
+LoadUnordered<mut::Morphology>::Iterator::operator++(int);
+
+template typename enable_if_mutable<mut::Morphology, std::pair<size_t, mut::Morphology>>::type
+    LoadUnordered<mut::Morphology>::Iterator::operator*<mut::Morphology>() const;
+
+template typename enable_if_immutable<Morphology, std::pair<size_t, Morphology>>::type
+    LoadUnordered<Morphology>::Iterator::operator*<Morphology>() const;
+
 
 }  // namespace morphio
--- tests/test_11_collection.py
+++ tests/test_11_collection.py
@@ -1,18 +1,27 @@
 from pathlib import Path
+import pytest
 
 import morphio
 from morphio import Collection
 
-DATA_DIR = Path(__file__).parent / "data"
+import numpy as np
 
-def check_load_from_collection(collection):
-    morphology_names = [
+DATA_DIR = Path(__file__).parent / "data"
+COLLECTION_PATHS = [DATA_DIR / "h5/v1/merged.h5", DATA_DIR / "h5/v1"]
+
+def available_morphologies():
+    return [
         "simple",
         "glia",
         "mitochondria",
         "endoplasmic-reticulum",
         "simple-dendritric-spine"
     ]
+
+
+def check_load_from_collection(collection):
+    morphology_names = available_morphologies()
+
     for morph_name in morphology_names:
         morph = collection.load(morph_name)
         assert isinstance(morph, morphio.Morphology)
@@ -24,12 +33,14 @@ def check_load_from_collection(collection):
         assert isinstance(morph, morphio.mut.Morphology)
 
 
-def check_load_from_collection_with_context(collection_path):
+@pytest.mark.parametrize("collection_path", COLLECTION_PATHS)
+def test_load_from_collection_with_context(collection_path):
     with morphio.Collection(collection_path) as collection:
         check_load_from_collection(collection)
 
 
-def check_load_from_collection_without_context(collection_path):
+@pytest.mark.parametrize("collection_path", COLLECTION_PATHS)
+def test_load_from_collection_without_context(collection_path):
     collection = morphio.Collection(collection_path)
 
     check_load_from_collection(collection)
@@ -37,12 +48,30 @@ def check_load_from_collection_without_context(collection_path):
     collection.close()
 
 
-def test_directory_collection():
-    collection_path = DATA_DIR / "h5/v1"
-    check_load_from_collection_with_context(collection_path)
-    check_load_from_collection_without_context(collection_path)
+@pytest.mark.parametrize("collection_path", COLLECTION_PATHS)
+def test_container_unordered(collection_path):
+    with morphio.Collection(collection_path) as collection:
+        morphology_names = available_morphologies()
+        morphology_names = morphology_names[1:]
 
-def test_container_collection():
-    collection_path = DATA_DIR / "h5/v1/merged.h5"
-    check_load_from_collection_with_context(collection_path)
-    check_load_from_collection_without_context(collection_path)
+        loop_indices = []
+        for k, morph in collection.load_unordered(morphology_names):
+            loop_indices.append(k)
+
+        np.testing.assert_array_equal(
+            sorted(loop_indices),
+            np.arange(len(morphology_names))
+        )
+
+
+@pytest.mark.parametrize("collection_path", COLLECTION_PATHS)
+def test_container_unordered(collection_path):
+    with morphio.Collection(collection_path) as collection:
+        morphology_names = available_morphologies()
+        morphology_names = morphology_names[1:]
+
+        loop_indices = collection.argsort(morphology_names)
+        np.testing.assert_array_equal(
+            sorted(loop_indices),
+            np.arange(len(morphology_names))
+        )
